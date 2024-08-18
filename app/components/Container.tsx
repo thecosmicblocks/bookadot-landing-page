cee20572f2b90683e8a05e68976aa4972e803476
duchuyhoang <64428807+duchuyhoang@users.noreply.github.com>
2024-08-18 16:05:59 +0700

    feat: implement landing page (#1)
    Co-authored-by: Huy Hoang <huyhd2@vmogroup.com>

--- app/components/Container.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from 'react'
-
-export function Container({ children, className }: { children: ReactNode, className?: string }) {
-  return (
-    <div className={`container ${className ??""}`}>{children}</div>
-  )
-}
