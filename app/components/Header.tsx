8f335ebcd22676439a6938678d48ed2a03adbf80
danielbui12 <huytung139@gmail.com>
2024-10-19 19:19:12 +0700

    update logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,53 +5,58 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 import { Logo } from "./Icon";
+import Image from "next/image";
 export const Header: React.FC<ComponentProps<any>> = ({ children }) => {
-  const pathname = usePathname();
-  console.log(pathname);
-  const navItems = [
-    {
-      title: "Home",
-      href: "/",
-    },
-    {
-      title: "About",
-      href: "/about",
-    },
-    {
-      title: "Contact",
-      href: "/contact",
-    },
-    {
-      title: "FAQ",
-      href: "/faq",
-    },
-  ];
-  return (
-    <Navbar fluid rounded className={"w-full rounded-none py-4"}>
-      <Navbar.Brand href="/">
-        <Logo />
-      </Navbar.Brand>
-      {/* <Navbar.Collapse> */}
-      <div
-        className={
-          "bookadot-header-item invisible flex flex-col justify-between gap-5 md:visible md:flex-row xl:gap-10"
-        }
-      >
-        {navItems.map((navItem, index) => (
-          <Link
-            href={navItem.href}
-            key={index}
-            className={twMerge(
-              "text-white hover:text-accent-color",
-              [navItem.href].includes(pathname) && "text-accent-color",
-            )}
-          >
-            {navItem.title}
-          </Link>
-        ))}
-      </div>
-      {/* </Navbar.Collapse> */}
-      <Button color={"bookadot-secondary"}>Launch dApp</Button>
-    </Navbar>
-  );
+    const pathname = usePathname();
+    const navItems = [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "About",
+            href: "/about",
+        },
+        {
+            title: "Contact",
+            href: "/contact",
+        },
+        {
+            title: "FAQ",
+            href: "/faq",
+        },
+    ];
+    return (
+        <Navbar fluid rounded className={"w-full rounded-none py-4"}>
+            <Navbar.Brand href="/">
+                <Image
+                    src="/logo.png"
+                    alt="Bookadot"
+                    width={65}
+                    height={65}
+                />
+            </Navbar.Brand>
+            {/* <Navbar.Collapse> */}
+            <div
+                className={
+                    "bookadot-header-item invisible flex flex-col justify-between gap-5 md:visible md:flex-row xl:gap-10"
+                }
+            >
+                {navItems.map((navItem, index) => (
+                    <Link
+                        href={navItem.href}
+                        key={index}
+                        className={twMerge(
+                            "text-white hover:text-accent-color",
+                            [navItem.href].includes(pathname) && "text-accent-color",
+                        )}
+                    >
+                        {navItem.title}
+                    </Link>
+                ))}
+            </div>
+            {/* </Navbar.Collapse> */}
+            <Button color={"bookadot-secondary"}>Launch dApp</Button>
+        </Navbar>
+    );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,35 +8,58 @@ import { Inter } from "next/font/google";
 import { Container } from "./components/Container/Container";
 
 const fontIter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
+    subsets: ["latin"],
+    variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
-  title: "Bookadot",
-  description: "Bookadot",
+    title: "Bookadot",
+    description: "Bookadot",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning className={fontIter.className}>
-      <head>
-        <ThemeModeScript />
-        <title>Bookadot</title>
-      </head>
-      <body className={"flex w-full flex-col items-center"}>
-        <Flowbite theme={{ mode: "dark", theme: themes }}>
-          <Container className={`min-h-[70vh] w-full p-5`}>
-            <Header></Header>
-            {children}
-            <TCBFooter></TCBFooter>
-          </Container>
-        </Flowbite>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning className={fontIter.className}>
+            <head>
+                <ThemeModeScript />
+                <title>Bookadot</title>
+                {/* Standard Favicon */}
+                <link rel="icon" type="image/jpeg" href="/logo.jpeg" />
+
+                {/* For iOS */}
+                <link rel="apple-touch-icon" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="120x120" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/logo.jpeg" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/logo.jpeg" />
+
+                {/* Android */}
+                <link rel="icon" sizes="192x192" href="/logo.jpeg" />
+                <link rel="icon" sizes="256x256" href="/logo.jpeg" />
+                <link rel="icon" sizes="384x384" href="/logo.jpeg" />
+                <link rel="icon" sizes="512x512" href="/logo.jpeg" />
+
+                {/* For Windows (pinned site) */}
+                <meta name="msapplication-TileImage" content="/logo.jpeg" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+
+                {/* General meta for theming */}
+                <meta name="theme-color" content="#ffffff" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0" />
+            </head>
+            <body className={"flex w-full flex-col items-center"}>
+                <Flowbite theme={{ mode: "dark", theme: themes }}>
+                    <Container className={`min-h-[70vh] w-full p-5`}>
+                        <Header></Header>
+                        {children}
+                        <TCBFooter></TCBFooter>
+                    </Container>
+                </Flowbite>
+            </body>
+        </html>
+    );
 }
